528a5edc08584e263cb4a2f6fb97b116ad0c8cb6
Yang Luo <992534544@qq.com>
2026-06-03 00:04:34 +0800

    Initial commit

---
+++ interfaces/plugin_base.hpp
@@ -0,0 +1,50 @@
+//
+// Created by think on 6/2/26.
+//
+
+#pragma once
+
+#include <string>
+
+#ifdef _WIN32
+#ifdef RPF_EXPORTS
+#define PLUGIN_API __declspec(dllexport)
+#else
+#define PLUGIN_API __declspec(dllimport)
+#endif
+#else
+#define PLUGIN_API __attribute__((visibility("default")))
+#endif
+
+class PluginBase {
+ public:
+  enum class State { Unloaded, Loaded, Initialized, Error };
+
+  enum class Type { Hardware, Planner, Controller, Kinematics, Dynamics };
+
+  virtual ~PluginBase() = default;
+
+  // 生命周期
+  virtual bool Initialize() = 0;
+  virtual bool Start() = 0;
+  virtual bool Stop() = 0;
+  virtual void Unload() = 0;
+
+  // 状态查询
+  [[nodiscard]] virtual State GetState() const = 0;
+  [[nodiscard]] virtual std::string GetName() const = 0;
+  [[nodiscard]] virtual std::string GetVersion() const = 0;
+};
+
+using PluginCreateFunc = PluginBase* (*)();
+using PluginDestroyFunc = void (*)(PluginBase*);
+
+// 宏定义，用于简化插件导出
+#define RPF_PLUGIN_EXPORT(PluginClass)                                  \
+  (                                                                     \
+      extern "C" PLUGIN_API PluginBase *                                \
+      create_plugin() {                                                 \
+        return new PluginClass();                                       \
+      } extern "C" PLUGIN_API void destroy_plugin(PluginBase* plugin) { \
+        delete plugin;                                                  \
+      })
---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef RPF_LIBRARY_H
+#define RPF_LIBRARY_H
+
+void hello();
+
+#endif // RPF_LIBRARY_H
